fb1b3247c1fc20f93cdc524a305b94a61ebba032
Alexander Harkness <bearbin@gmail.com>
2014-04-04 09:59:08 +0100

    Git push for debugging.

--- mcgorcon.go
+++ mcgorcon.go
@@ -6,18 +6,14 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
-	"io"
 	"math/rand"
 	"net"
 	"time"
 )
 
-const (
-	PACKET_TYPE_COMMAND  int32 = 2
-	PACKET_TYPE_AUTH     int32 = 3
-	REQUEST_ID_BAD_LOGIN int32 = -1
-	PADDING_TWO_BYTES    [2]byte
-)
+const PACKET_TYPE_COMMAND int32 = 2
+const PACKET_TYPE_AUTH int32 = 3
+const REQUEST_ID_BAD_LOGIN int32 = -1
 
 type Client struct {
 	password   string
@@ -40,13 +36,8 @@ func Dial(host string, port int, pass string) Client {
 	if err != nil {
 		panic(err)
 	}
-	// Set a timeout for read and write.
-	err = conn.SetDeadline(10 * time.Second)
-	if err != nil {
-		panic(err)
-	}
 	// Create the client object, since the connection has been established.
-	c = Client{password: pass, connection: conn}
+	c := Client{password: pass, connection: conn}
 	// TODO - server validation to make sure we're talking to a real RCON server.
 	// For now, just return the client and assume it's a real server.
 	return c
@@ -83,34 +74,36 @@ func (c *Client) Authenticate() {
 // sendPacket sends the binary packet representation to the server and returns the response.
 func (c *Client) sendPacket(packet []byte) []byte {
 	// Send the packet over the wire.
-	wn, err := c.connection.Write(packet)
+	_, err := c.connection.Write(packet)
 	if err != nil {
-		panic(err)
+		panic("WRTE FAIL")
 	}
 	// Get a response.
-	var obuf [4096]byte
-	rn, err := c.connection.Read(obuf)
-	if err != nil {
-		panic(err)
-	}
+	var obuf []byte
+	_, err = c.connection.Read(obuf)
+	//if err != nil {
+	//	panic("RDE FAILE")
+	//}
+	return obuf
 }
 
 // packetise encodes the packet type and payload into a binary representation to send over the wire.
 func packetise(t int32, p []byte) []byte {
 	// Generate a random request ID.
-	ID = requestID()
+	ID := requestID()
+	pad := [2]byte{}
 	var buf bytes.Buffer
-	binary.Write(buf, binary.LittleEndian, ID)
-	binary.Write(buf, binary.LittleEndian, t)
-	binary.Write(buf, binary.LittleEndian, p)
-	binary.Write(buf, binary.LittleEndian, PADDING_TWO_BYTES)
+	binary.Write(&buf, binary.LittleEndian, ID)
+	binary.Write(&buf, binary.LittleEndian, t)
+	binary.Write(&buf, binary.LittleEndian, p)
+	binary.Write(&buf, binary.LittleEndian, pad)
 	payload := buf.Bytes()
 	// Get the length of the payload.
-	var length int32 = len(payload)
+	length := int32(len(payload))
 	// Assemble the full buffer now.
 	buf.Reset()
-	binary.Write(buf, binary.LittleEndian, length)
-	binary.Write(buf, binary.LittleEndian, payload)
+	binary.Write(&buf, binary.LittleEndian, length)
+	binary.Write(&buf, binary.LittleEndian, payload)
 	// Notchian server doesn't like big packets :(
 	if buf.Len() >= 1460 {
 		panic("Packet too big when packetising.")
@@ -121,12 +114,13 @@ func packetise(t int32, p []byte) []byte {
 
 // depacketise decodes the binary packet into a native Go struct.
 func dePacketise(raw []byte) packet {
-	buf := bytes.NewBuffer(raw[:])
+	buf := bytes.NewBuffer([]byte(raw[:]))
 	pack := packet{}
-	err := binary.Read(raw, binary.LittleEndian, &pack)
+	err := binary.Read(buf, binary.LittleEndian, &pack)
 	if err != nil {
 		panic(err)
 	}
+	pack.Payload = pack.Payload[:len(pack.Payload)-2]
 	return pack
 }
 
